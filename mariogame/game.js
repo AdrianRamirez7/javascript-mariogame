bd10c63355f7548c554357e59851297138c30b1e
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2025-03-16 21:46:59 -0700

    modified javascript

---
+++ mariogame/game.js
@@ -0,0 +1,12 @@
+const {Client, Account, Databases, ID, Query} = Appwrite
+
+const projectid = '67ce1e1e00136dc21821'
+const database = ''
+const collectionId = ''
+
+const client = new Client()
+
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(projectid)
+
+    const account
